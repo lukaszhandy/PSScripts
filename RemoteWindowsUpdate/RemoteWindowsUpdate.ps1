450e2c4f29a6efd5dc9d52eb960c9edc57d115e2
lukaszhandy <66007288+lukaszhandy@users.noreply.github.com>
2020-08-21 13:50:41 +0200

    Add files via upload

---
+++ RemoteWindowsUpdate/RemoteWindowsUpdate.ps1
@@ -0,0 +1,19 @@
+﻿$computer = Read-host "podaj nazwę komputera(ów): "
+$computer+='*'
+$computer
+
+$sessions = New-PSSession -ComputerName (Get-ADComputer -Filter * | where name -Like $computer | select -expandProperty name)
+
+$sessions
+
+ Invoke-Command -Session $sessions {
+#C:\Windows\System32\USOClient.exe StartInteractiveScan    
+#C:\Windows\System32\USOClient.exe startdownload   
+#C:\Windows\System32\USOClient.exe StartInstall 
+#C:\Windows\System32\USOClient.exe ScanInstallWait    
+#C:\Windows\System32\USOClient.exe RestartDevice    
+#C:\Windows\System32\USOClient.exe ResumeUpdate    
+#C:\Windows\System32\USOClient.exe Refreshsettings   
+}
+
+Get-PSSession | Remove-PSSession
