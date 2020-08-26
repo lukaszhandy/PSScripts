f87f72a63dd9d41ec7847021d638b96ffb7d210f
lukaszhandy <66007288+lukaszhandy@users.noreply.github.com>
2020-08-26 07:09:13 +0200

    Add files via upload

---
+++ RestartComputerAfterXXSeconds/RestartComputerAfterXXSeconds.ps1
@@ -0,0 +1,19 @@
+﻿cls
+$Date = Get-Date
+do{
+sleep(60);
+$Date1 = Get-Date ;
+
+$DiffDate = $Date1 - $Date;
+
+[int]$res=[convert]::ToInt32($DiffDate.TotalMinutes);
+
+$res;
+
+if($res -eq 200)
+{
+  Restart-Computer -Force
+}
+
+}
+while(1-eq1)
